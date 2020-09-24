7c5de2c2a9c3323f2cf64b90af35d017999b0785
sukuna2713 <sukuna2713@gmail.com>
2020-09-24 07:55:23 +0000

    set ts

--- front-study/assessment.js
+++ front-study/assessment.js
@@ -1,9 +1,20 @@
-'use strict';
-const answers = [
+var userNameInput = document.getElementById('user-name');
+var assessmentButton = document.getElementById('assessment');
+var resultDivided = document.getElementById('result-area');
+var tweetDivided = document.getElementById('tweet-area');
+assessmentButton.onclick = function() {
+  console.log('ボタンが押されました');
+  // TODO 診断結果表示エリアの作成
+  // TODO ツイートエリアの作成
+};
+var answers = [
     '{username}unk',
     '{username}tnk'
-]
-
+];
 function assessment(username) {
-
+    // 全文字のコード番号を取得してそれを足し合わせる
+    var sumOfCharCode = username.split("").map(function (i) { return i.charCodeAt(0); }).reduce(function (acc, v) { return acc + v; }, 0);
+    var index = sumOfCharCode % answers.length;
+    var answer = answers[index].replace(/\{username\}/g, username);
+    return answer;
 }
--- front-study/assessment.ts
+++ front-study/assessment.ts
@@ -1,4 +1,18 @@
-const answers: string = [
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resultDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+const answers: string[] = [
     '{username}unk',
     '{username}tnk'
-]
+];
+
+function assessment(username: string): string {
+    // 全文字のコード番号を取得してそれを足し合わせる
+    const sumOfCharCode: number = username.split("").map(i => i.charCodeAt(0)).reduce((acc,v) => {return acc + v}, 0);
+
+    const index = sumOfCharCode % answers.length;
+    const answer = answers[index].replace(/\{username\}/g, username);
+
+    return answer
+}
